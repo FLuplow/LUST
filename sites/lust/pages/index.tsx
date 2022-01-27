4c85d5d0f0dd4cb5198b4e54bbfb027a2c37ff85
FLuplow <felix.luplow@code.berlin>
2022-01-28 00:05:23 +0100

    LUSTify

--- sites/lust/pages/index.tsx
+++ sites/lust/pages/index.tsx
@@ -1,49 +1,13 @@
-import { useEffect, useState } from 'react';
-import styles from './index.module.scss';
-import TextTransition, { presets } from 'react-text-transition';
-import { useLogin, useSupabase } from '../hooks/supabase';
-import { Link } from '@snowstorm/core';
-
-const t = [
-	'bouldering',
-	'running',
-	'camping',
-	'on festivals',
-	'on raves',
-	'in bars',
-	'partying',
-];
+import React from "react";
+import styles from "./index.module.scss";
 
 export const Index = () => {
-	const login = useLogin();
-	const [index, setIndex] = useState<number>(0);
-
-	useEffect(() => {
-		const intervalId = setInterval(() => setIndex(i => i + 1), 3000);
-		return () => clearTimeout(intervalId);
-	}, []);
-
-	return (
-		<div className={styles.landingpage}>
-			<div className={styles.hero}>
-				<h1>JustMeet</h1>
-				<h2>
-					Meet new friends{' '}
-					<TextTransition
-						text={t[index % t.length]}
-						inline
-						springConfig={presets.default}
-					/>
-				</h2>
-				<div className={styles.actions}>
-					<button type="button" onClick={() => login.mutate()}>
-						Authenticate with Discord
-					</button>
-					<Link to="/faq">
-						<button type="button">FAQ</button>
-					</Link>
-				</div>
-			</div>
-		</div>
-	);
+  return (
+    <div className={styles.landingpage}>
+      <div className={styles.hero}>
+        <h1>Real Itätsverlust</h1>
+        <img src="../assets/lust_large.png" alt="LUST" />
+      </div>
+    </div>
+  );
 };
