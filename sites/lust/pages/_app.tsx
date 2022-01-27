c757d3d79884e42d73358a0d844074adc1fbc31a
FLuplow <felix.luplow@code.berlin>
2022-01-28 00:15:12 +0100

    stuff

--- sites/lust/pages/_app.tsx
+++ sites/lust/pages/_app.tsx
@@ -1,15 +1,16 @@
-import { SnowstormCustomAppProps } from '@snowstorm/core/types';
-import { QueryClient, QueryClientProvider } from 'react-query';
-import { Layout } from '../components/layout';
-import { SupabaseContext } from '../hooks/supabase';
+import React from 'react'
+import { SnowstormCustomAppProps } from '@snowstorm/core/types'
+import { QueryClient, QueryClientProvider } from 'react-query'
+import { Layout } from '../components/layout'
+import { SupabaseContext } from '../hooks/supabase'
 
-const queryClient = new QueryClient();
+const queryClient = new QueryClient()
 
 const App = ({ children }: SnowstormCustomAppProps) => (
-	<Layout>
-		<QueryClientProvider client={queryClient}>
-			<SupabaseContext>{children}</SupabaseContext>
-		</QueryClientProvider>
-	</Layout>
-);
-export default App;
+  <Layout>
+    <QueryClientProvider client={queryClient}>
+      <SupabaseContext>{children}</SupabaseContext>
+    </QueryClientProvider>
+  </Layout>
+)
+export default App
