2b8315b8e153a77a60c161e7ec1bd48a7ea8602d
FLuplow <felix.luplow@code.berlin>
2022-01-28 00:20:35 +0100

    fix img import

--- sites/lust/pages/index.tsx
+++ sites/lust/pages/index.tsx
@@ -1,13 +1,15 @@
-import React from "react";
-import styles from "./index.module.scss";
+import React from 'react'
+import styles from './index.module.scss'
+
+import logo from '../assets/lust_large.png'
 
 export const Index = () => {
   return (
     <div className={styles.landingpage}>
       <div className={styles.hero}>
         <h1>Real Itätsverlust</h1>
-        <img src="../assets/lust_large.png" alt="LUST" />
+        <img src={logo} alt="LUST" />
       </div>
     </div>
-  );
-};
+  )
+}
