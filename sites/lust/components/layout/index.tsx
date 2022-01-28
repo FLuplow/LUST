612efd6a51020f6a9cc8db92d321e93393472a25
FLuplow <felix.luplow@code.berlin>
2022-01-28 11:32:56 +0100

    add: built with snowstorm

--- sites/lust/components/layout/index.tsx
+++ sites/lust/components/layout/index.tsx
@@ -5,6 +5,14 @@ import './global.css'
 export const Layout: React.FC = ({ children }) => (
   <div className={styles.layout}>
     <div className={styles.content}>{children}</div>
-    <div className={styles.footer}></div>
+    <div className={styles.footer}>
+      {' '}
+      <span>
+        built with{' '}
+        <a href="https://snowstorm.js.org" target="_blank" rel="noreferrer">
+          Snowstorm
+        </a>
+      </span>
+    </div>
   </div>
 )
